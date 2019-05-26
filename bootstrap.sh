7b6a3273977a4818b38a44200c027fe998aeda2a
Markus Hilbert <markus.hilbert@iham.at>
2019-05-26 11:02:00 +0200

    does it work with default types?

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./local ./bin
-ln -fs plone-5.1.x.cfg buildout.cfg
+ln -fs plone.cfg buildout.cfg
 virtualenv --clear .
 ./bin/pip install --upgrade pip setuptools zc.buildout
 ./bin/buildout
